b8001dfe15ddc233c21626c8a16ed43d0a4fd30c
easydaniel <whitetiger50512@gmail.com>
2015-10-01 12:14:24 +0800

    easy

---
+++ statements/statements.py
@@ -0,0 +1,69 @@
+
+### Variables Declaration
+
+# var = whatever you want
+
+### Basic I/O (Input/Output)
+
+## Core function: print, input
+
+#var = input('What is this? ')
+#print(var)
+
+### print format
+
+#var = 123.12345
+#print("%05d" % var)
+#print("%.3f" % var)
+
+### Datatypes: int, float, str, list, tuple, dict, bool
+## special type None
+
+#print(type(1))
+#print(type(1.0))
+#print(type('1'))
+#print(type((1,1)))
+#print(type([1]))
+#print(type({1:1}))
+#print(type(1==1))
+
+### Operation: + - * / // ** %
+
+#print(1/2)
+#print(1/2.0)
+#print(5.0/2)
+#print(5.0//2)
+#print(5**2)
+#print(5%2)
+
+### Functions
+
+#def funcName (paramOne, paramTwo):
+    #return paramOn + paramTwo
+
+#print(funcName(1,2))
+
+### Modules: import somemodules
+
+#import math
+#import random
+
+#print(math.pi)
+#print(random.random())
+#print(random.random())
+
+
+### Conditions
+
+## Logics == != or not < > <= >=
+
+#conOne = True
+#conTwo = False
+
+#if (conOne):
+    #print('Condition 1')
+#elif (conTwo):
+    #print('Condition 2')
+#else:
+    #print('Rest of them')
+
