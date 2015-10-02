a94f481ca1c5b06873c8bf06cf9905bcf6920c32
allenwhale <allenwhale@yahoo.com.tw>
2015-10-03 02:25:13 +0800

    update

--- var.py
+++ var.py
@@ -1,37 +1,47 @@
-num1 = 1
+### section 1 ###
+num = 10
 def func1():
-    print num1
+    num = 20
+    print(num) 
 
+func1() 
+print(num)
+'''
+output:
+20
+10
+'''
+
+
+
+### section 2 ###
+num = 10
 def func2():
-    global num1
-    #num1 = 10
-    print num1
+    global num
+    print(num)
 
+func2()
+print(num)
+'''
+output:
+10
+10
+'''
+
+
+
+### section 3 ###
+num = 10
 def func3():
-    num1 = 10
-    print num1
+    global num
+    num = 20
+    print(num)
 
-def func4():
-    global num2
-    num2 = 10
-    print num2
-
-def func5():
-    print num2
-
-def func6():
-    num2 = 20
-    print num2
-
-def func7():
-    for i in range(5):
-        for j in range(i):
-            print 'inside', j
-        print 'outide', i
-
-def func8():
-    for i in range(5):
-        for i in range(i):
-            print 'inside', i
-        print 'outside', i
-
+func3()
+print(num)
+'''
+output:
+20
+20
+'''
+print(num)
